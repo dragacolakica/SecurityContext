5975ebe39d2682aad6c40f59ffe3139851d126cd
Dragica Čolak <dragica.colak@infodom.hr>
2022-01-10 11:15:26 +0100

    Add project files.

---
+++ WebApp_UnderTheHood/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace WebApp_UnderTheHood.Pages.Acount
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential Credential { get; set; }
+        public void OnGet()
+        {
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+            if (Credential.Username == "admin" && Credential.Password == "password")
+            {
+                // Creating security context
+                var claims = new List<Claim> {
+                new Claim(ClaimTypes.Name, "admin"),
+                new Claim(ClaimTypes.Email, "admin@test.com"),
+                new Claim("Department", "HR"),
+                new Claim("Admin", "true"),
+                new Claim("Manager", "true"),
+                new Claim("EmploymentDate", "2021-05-01")
+                };
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+                var authProperties = new AuthenticationProperties
+                {
+                    IsPersistent = Credential.RememberMe
+                };
+
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+                return RedirectToPage("/Index");
+            }
+            return Page();
+        }
+    }
+
+    public class Credential
+    {
+        [Required]
+        [Display(Name = "User Name")]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Display (Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ WebApp_UnderTheHood/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp_UnderTheHood.Pages
+{
+    [Authorize]
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
